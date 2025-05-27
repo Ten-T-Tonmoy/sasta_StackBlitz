a5ef7a55d27a26c825df6715342acf8758e7b6c8
Ten-T-Tonmoy <stu01754@gmail.com>
2025-05-27 13:49:20 +0600

    bottombar resize logic wip

--- client/src/pages/Editor/Editor.jsx
+++ client/src/pages/Editor/Editor.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import axios from "axios";
 
 import { IoPlayOutline } from "react-icons/io5";
 
 const CodeEditor = () => {
+  //for lang and theme choosing
   const [language, setLanguage] = useState("cpp");
   const [theme, setTheme] = useState("vs-dark");
 
+  //for input and output
   const [code, setCode] = useState("// set back relax and kaboom!");
   const [output, setOutput] = useState("");
 
@@ -24,8 +26,61 @@ const CodeEditor = () => {
       console.log(error);
     }
   };
+
+  //for resizeable splitter/split plane side panel
+  const minSideWidth = 150;
+  const minBottomHeight = 150;
+
+  const [sideBarWidth, setSideBarWidth] = useState(300); //for pixel perfection man
+  const [bottomBarHeight, setBottomBarHeight] = useState(170);
+  const isChanging = useRef(null);
+
+  const startSideChange = () => {
+    isChanging.current = "sideBar";
+    document.body.style.cursor = "col-resize";
+  };
+
+  const startBottomChange = () => {
+    isChanging.current = "bottomBar";
+    document.body.style.cursor = "row-resize";
+  };
+
+  const stopChange = () => {
+    isChanging.current = null;
+    document.body.style.cursor = "default";
+  };
+
+  const handleChange = (e) => {
+    if (!isChanging.current) return;
+    //X axis will be for vertical Y will be for horizontal
+
+    if (isChanging.current === "sideBar") {
+      setSideBarWidth(Math.max(e.clientX, minSideWidth));
+    } else if (isChanging.current === "bottomBar") {
+      //from top calc it suks af
+
+      const newHeight = window.innerHeight - e.clientY;
+      if (newHeight < 0.8 * window.innerHeight) {
+        setBottomBarHeight(Math.max(newHeight, minBottomHeight));
+      }
+    }
+  };
+
+  //for splitter horizental
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleChange);
+    window.addEventListener("mouseup", stopChange);
+
+    return () => {
+      //event listeners otherwise stays forever?
+      window.removeEventListener("mousemove", handleChange);
+      window.removeEventListener("mouseup", stopChange);
+    };
+  }, []);
+
   return (
-    <div className="">
+    <div className="h-[100vh] w-[100vw] overflow-hidden">
       <div
         className="
       h-[5vh] bg-gray-900 items-center flex justify-between text-[.8rem]"
@@ -49,20 +104,47 @@ const CodeEditor = () => {
         </div>
       </div>
 
-      <div className="flex">
-        <div className="w-20vw">This is reserved for sidebar only yeah</div>
+      <div
+        style={{ height: `calc(95vh - ${bottomBarHeight}px - 4px)` }}
+        className="flex "
+      >
+        <div
+          className=""
+          style={{
+            width: `${sideBarWidth}px`,
+          }}
+        >
+          This is reserved for sidebar only yeah
+        </div>
+        <div
+          onMouseDown={startSideChange}
+          className="border hover:w-1 hover:cursor-col-resize border-gray-700/40 hover:bg-blue-500 "
+        ></div>
         <Editor
-          width="80vw"
-          height="75vh"
+          width={`calc(100vw - ${sideBarWidth}px - 4px)`}
+          height={`calc(95vh-${bottomBarHeight}px - 4px)`}
           language={language}
+          defaultLanguage="javascript"
           value={code}
           theme={theme}
           onChange={(val) => setCode(val || "")}
         />
       </div>
-      <div className="h-[20vh] overflow-y-auto font-[50] text-[.9rem] px-4 py-1  bg-gray-900 ">
+      <div
+        onMouseDown={startBottomChange}
+        className="hover:h-[6px] h-1  hover:cursor-row-resize border-gray-700 border-t-1
+         hover:bg-blue-500"
+      ></div>
+      <div
+        style={{
+          height: bottomBarHeight,
+        }}
+        className=" overflow-y-auto font-[50] text-[.9rem] px-4   bg-gray-900 "
+      >
         output:
-        <p className="text-green-400 whitespace-pre-wrap ">{output}</p>
+        <p className="text-green-400 px-[10vw] text-[.7rem] whitespace-pre-wrap ">
+          {output}
+        </p>
       </div>
     </div>
   );
